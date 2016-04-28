a6784b148d6be65ee7bc9e25953765f755de2645
DylanFayant <dylan.fayant@iut-valence.fr>
2016-04-28 15:23:23 +0200

    Creating the first classes
    Creating and initialising the main with thr two main classes (Room and
    Calendar)

--- src/fr/iutvalence/info/dut/m2107/Main.java
+++ src/fr/iutvalence/info/dut/m2107/Main.java
@@ -1,10 +1,19 @@
 package fr.iutvalence.info.dut.m2107;
 
+/**
+ * Application that launches an IHM, a Calendar and a Room 
+ * @author Projet Resto
+ *
+ */
 public class Main {
-
+	
+	/**
+	 * Application's main
+	 * @param args command-line arguments (unused)
+	 */
 	public static void main(String[] args) {
-		
-
+		Calendar calendar = new Calendar();
+		Room room = new Room();
 	}
 
 }
