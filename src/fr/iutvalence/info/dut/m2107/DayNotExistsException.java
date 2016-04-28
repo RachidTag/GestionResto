d17f58935c65f7c2d7cb3676507af8f95d43d642
DylanFayant <dylan.fayant@iut-valence.fr>
2016-04-28 15:54:12 +0200

    Creation of the Day class and Coding the Week class
    Major coding of the week class and creation of the day class.
    Creation of the exception linked to the Week class.

---
+++ src/fr/iutvalence/info/dut/m2107/DayNotExistsException.java
@@ -0,0 +1,10 @@
+package fr.iutvalence.info.dut.m2107;
+
+/**
+ * The day isn't existing
+ * @author Projet Resto
+ *
+ */
+public class DayNotExistsException extends Exception {
+
+}
--- src/fr/iutvalence/info/dut/m2107/Week.java
+++ src/fr/iutvalence/info/dut/m2107/Week.java
@@ -1,5 +1,47 @@
 package fr.iutvalence.info.dut.m2107;
 
-public class Week {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Represents a week from the calendar
+ * @author Projet Resto
+ *
+ */
+public class Week {
+	
+	/**
+	 * List of the days identified from 1 to 7
+	 */
+	private List<Day> days = new ArrayList<Day>();
+	
+	/**
+	 * Generates a week with 7 days
+	 */
+	public Week() {
+		for(int i = 1; i <= 7; i++)
+			days.set(i, new Day());
+	}
+	
+	/**
+	 * @param dayNum the num of the day (between 1 and 7)
+	 * @return the selected day
+	 * @throws DayNotExistsException
+	 */
+	public Day getDay(int dayNum) throws DayNotExistsException
+	{
+		if(this.days.get(dayNum) == null) throw new DayNotExistsException();
+		return this.days.get(dayNum);
+	}
+	
+	/**
+	 * @param dayNum weekNum the number of the week from the year
+	 * @param Day a Day
+	 * @throws DayNotExistsException
+	 */
+	public void editDayCalendar(int dayNum, Day Day) throws DayNotExistsException
+	{
+		if(this.days.get(dayNum) == null) throw new DayNotExistsException();
+		this.days.set(dayNum, Day);
+	}
 }
