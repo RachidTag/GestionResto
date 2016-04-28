decd24d11fa7e4d0b982a4d67b0d900ddc7d5d42
DylanFayant <dylan.fayant@iut-valence.fr>
2016-04-28 22:12:46 +0200

    Beginning the code of the Day class

--- src/fr/iutvalence/info/dut/m2107/Day.java
+++ src/fr/iutvalence/info/dut/m2107/Day.java
@@ -1,5 +1,42 @@
 package fr.iutvalence.info.dut.m2107;
 
+/**
+ * Represents a day in a week
+ * @author Projet Resto
+ *
+ */
 public class Day {
+	
+	/**
+	 * If the restaurant is closed today
+	 */
+	private boolean isClosed;
+	
+	// TODO : The services
+	
+	/**
+	 * Generates a day with default value(s)
+	 */
+	public Day()
+	{
+		this.isClosed = false;
+	}
+
+	/**
+	 * @return true if the restaurant is closed, false if it's not
+	 */
+	public boolean isClosed() {
+		return this.isClosed;
+	}
+
+	/**
+	 * Set the state of the restaurant
+	 * @param isClosed
+	 */
+	public void setClosed(boolean isClosed) {
+		this.isClosed = isClosed;
+	}
+	
+	
 
 }
