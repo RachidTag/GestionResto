da0453b50229c10af8e2a954254005979d71a2e8
DylanFayant <dylan.fayant@iut-valence.fr>
2016-05-02 15:59:33 +0200

    Creation of the Table class, need to be finished

---
+++ src/fr/iutvalence/info/dut/m2107/Table.java
@@ -0,0 +1,33 @@
+package fr.iutvalence.info.dut.m2107;
+/**
+ * Represent a table
+ * @author Prejoet Resto
+ */
+public class Table
+{
+	/**
+	 * Auto-Incrementation number
+	 */
+	private static int autoIncrementation = 1;
+	/**
+	 * Number of the table
+	 */
+	private final int numTable;
+	/**
+	 * Number of places for the table
+	 */
+	private int numberPlaces;
+	/**
+	 * Position of the table in the sector
+	 */
+	private Position position;
+	/**
+	 * State of the table
+	 */
+	private State state;
+	/**
+	 * State of the meal
+	 */
+	// TODO atributes and methodes
+	
+}
