b30777992734edfd010f1bb20ece2b3776cf869a
DylanFayant <dylan.fayant@iut-valence.fr>
2016-05-04 13:30:29 +0200

    Javadoc

--- src/fr/iutvalence/info/dut/m2107/room/ClientNameRequiredException.java
+++ src/fr/iutvalence/info/dut/m2107/room/ClientNameRequiredException.java
@@ -1,5 +1,10 @@
 package fr.iutvalence.info.dut.m2107.room;
 
+/**
+ * Client name is required to add a reservation
+ * @author Projet Resto
+ *
+ */
 public class ClientNameRequiredException extends Exception
 {
 
