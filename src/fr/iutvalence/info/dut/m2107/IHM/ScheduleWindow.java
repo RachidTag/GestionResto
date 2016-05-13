8b041d4f640926e035f1886db333ff094a6ce0f2
Narffy <Narffy@Narffy-PC>
2016-05-14 00:02:17 +0200

    Improving the schedule Window

--- src/fr/iutvalence/info/dut/m2107/IHM/ScheduleWindow.java
+++ src/fr/iutvalence/info/dut/m2107/IHM/ScheduleWindow.java
@@ -1,6 +1,10 @@
 package fr.iutvalence.info.dut.m2107.IHM;
 
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class ScheduleWindow extends JFrame{
@@ -23,6 +27,22 @@ public class ScheduleWindow extends JFrame{
 		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
+		
+		GridLayout week = new GridLayout(3,7);
+		
+		JPanel Day = new JPanel();
+		
+		Day.setLayout(week);
+		this.add(Day);
+		Day.add(new JLabel("Monday"));
+		Day.add(new JLabel("Tuesday"));
+		Day.add(new JLabel("Wednesday"));
+		Day.add(new JLabel("Thursday"));
+		Day.add(new JLabel("Friday"));
+		Day.add(new JLabel("Saturday"));
+		Day.add(new JLabel("Sunday"));
+		
+		
 		
 		this.setVisible(true);
 	}
