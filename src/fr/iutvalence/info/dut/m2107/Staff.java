4edc6f49641a04480f13941ef2342ad78bb420f5
Théo <Tho@192.168.1.12>
2016-05-21 15:47:04 +0200

    Start saving and loading method

--- src/fr/iutvalence/info/dut/m2107/Staff.java
+++ src/fr/iutvalence/info/dut/m2107/Staff.java
@@ -1,8 +1,12 @@
 package fr.iutvalence.info.dut.m2107;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import fr.iutvalence.info.dut.m2107.room.ObjectReadedIsNotARoomException;
+import fr.iutvalence.info.dut.m2107.room.Room;
 import fr.iutvalence.info.dut.m2107.room.Sector;
 import fr.iutvalence.info.dut.m2107.room.SectorAlreadyExistsException;
 import fr.iutvalence.info.dut.m2107.room.SectorNotExistsException;
@@ -57,12 +61,63 @@ public class Staff {
 	
 	/**
 	 * add an unique waiter
-	 * @param waiter Sector
+	 * @param waiter Waiter
 	 * @throws SectorAlreadyExistsException 
 	 */
 	public void addSector(Waiter waiter) throws SectorAlreadyExistsException{
 		if(this.waiters.containsValue(waiter)) throw new SectorAlreadyExistsException();
 		this.waiters.put(waiter.getNumWaiter(), waiter);
 	}
+	
+	/**
+	 * edit an unique Waiter
+	 * @param numWaiter a num of Sector
+	 * @param waiter Waiter
+	 * @throws WaiterDoesNotExistException 
+	 */
+	public void editSector(int numWaiter, Waiter waiter) throws WaiterDoesNotExistException{
+		if(!this.waiters.containsKey(numWaiter)) throw new WaiterDoesNotExistException();
+		this.waiters.put(numWaiter, waiter);
+	}
+
+	/**
+	 * delete a waiters
+	 * @param numWaiter int
+	 * @throws WaiterDoesNotExistException 
+	 */
+	public void deleteSector(int numWaiter) throws WaiterDoesNotExistException{
+		if(!this.waiters.containsKey(numWaiter)) throw new WaiterDoesNotExistException();
+		this.waiters.remove(numWaiter);
+	}
+	
+	/**
+	 * Save the current room to the file savingRoom.save
+	 */
+	public void saveStaff() {
+		saveStaff("savingStaff.save");
+	}
+
+	private void saveStaff(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * Load a staff from savingStaff.save
+	 * @return staffReaded
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws ObjectReadedIsNotARoomException
+	 */
+	public static Room loadStaff() throws FileNotFoundException, IOException, ClassNotFoundException, ObjectReadedIsNotARoomException {
+		return loadStaff("savingRoom.save");
+	}
+
+	private static Room loadStaff(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	
 }
