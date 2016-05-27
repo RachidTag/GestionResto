dc4f36d5827ef58e5259acf371a4e639e07d727c
aubem <aubem@PC-B123-09.iut-valence.fr>
2016-05-27 08:58:08 +0200

    added resizable images, need to be improved - Mathieu

--- src/fr/iutvalence/info/dut/m2107/IHM/CustomButton/CustomButton.java
+++ src/fr/iutvalence/info/dut/m2107/IHM/CustomButton/CustomButton.java
@@ -4,8 +4,10 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontFormatException;
+import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
@@ -21,10 +23,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class CustomButton extends JButton implements MouseListener {
-	private Image buttonBg;
+	private ImageIcon buttonBg;
 	
 		 
-	public CustomButton(String name){
+	public CustomButton(String name, Dimension dimension){
 		Font customFont = null;
 		try {
 		    //create the font to use. Specify the size!
@@ -38,8 +40,8 @@ public class CustomButton extends JButton implements MouseListener {
 		    e.printStackTrace();
 		}
 		
-		ImageIcon bgImage = new ImageIcon(getClass().getResource("ButtonBackground.png"));
-		this.setIcon(bgImage);
+		buttonBg = new ImageIcon(getClass().getResource("ButtonBackground.png"));
+		this.setIcon(buttonBg);
 		
 		JLabel jLabelName = new JLabel(name, JLabel.CENTER);
 		jLabelName.setFont(customFont);
@@ -51,7 +53,7 @@ public class CustomButton extends JButton implements MouseListener {
 		this.setBorderPainted(false);
 		this.setFocusPainted(false);
 		this.addMouseListener(this);
-		this.setPreferredSize(new Dimension(200, 70));
+		this.setPreferredSize(dimension);
 	}
 	
 	
@@ -72,9 +74,9 @@ public class CustomButton extends JButton implements MouseListener {
 		 * Création du bouton, application d'une taille par défault pour ce bouton
 		 */
 		
-		CustomButton bouton = new CustomButton("Edit Table");
-		CustomButton bouton2 = new CustomButton("Edit TRUC");
-		CustomButton bouton3 = new CustomButton("GOD MOD");
+		CustomButton bouton = new CustomButton("Edit Table", new Dimension(90,150));
+		CustomButton bouton2 = new CustomButton("Edit TRUC", new Dimension(200,70));
+		CustomButton bouton3 = new CustomButton("GOD MOD", new Dimension(20,80));
 		
 		pan.add(bouton);
 		pan.add(bouton2);
@@ -85,6 +87,101 @@ public class CustomButton extends JButton implements MouseListener {
 		window.setVisible(true);
 		
 
+	}
+	
+	public static BufferedImage load9Patch(String path, int reqWidth, int reqHeight)
+    {
+        int x1=0,x2,y1=0,y2,w,h;
+        BufferedImage sourceImage=null;
+        BufferedImage img[][];
+        img = new BufferedImage[3][3];
+         
+        try {
+            sourceImage = ImageIO.read(new File(path));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        } 
+         
+         
+        w=sourceImage.getWidth();
+        h=sourceImage.getHeight();
+        x2=w-1;
+        y2=h-1;
+         
+        while((((sourceImage.getRGB(x1, 0)) >>24) & 0xff) == 0 && x1<x2)//on cherche le 1er pixel non transparent
+        {
+            x1++;
+        }
+         
+        if(x1!=x2)
+        {
+            while((((sourceImage.getRGB(x2, 0)) >>24) & 0xff) == 0)
+            {
+                x2--;
+            }
+        }
+         
+        while((((sourceImage.getRGB(0, y1)) >>24) & 0xff) == 0 && y1<y2)
+        {
+            y1++;
+        }
+         
+        if(y1!=y2)
+        {
+            while((((sourceImage.getRGB(0, y2)) >>24) & 0xff) == 0)
+            {
+                y2--;
+            }
+        }
+         
+        if(x1==x2 && y1==y2)return sourceImage;//aucune ligne trouvée
+         
+        img[0][0]=sourceImage.getSubimage(1, 1, x1, y1);
+        img[1][0]=sourceImage.getSubimage(1+x1, 1, x2-x1-1, y1);  
+        img[2][0]=sourceImage.getSubimage(1+x2, 1, w-x2-1, y1);
+        img[0][1]=sourceImage.getSubimage(1, 1+y1, x1, y2-y1);  
+        img[1][1]=sourceImage.getSubimage(1+x1, 1+y1, x2-x1-1, y2-y1);  
+        img[2][1]=sourceImage.getSubimage(1+x2, 1+y1, w-x2-1, y2-y1);
+        img[0][2]=sourceImage.getSubimage(1, 1+y2, x1, h-y2-1);
+        img[1][2]=sourceImage.getSubimage(1+x1, 1+y2, x2-x1-1, h-y2-1);
+        img[2][2]=sourceImage.getSubimage(1+x2, 1+y2, w-x2-1, h-y2-1);
+         
+        int verW = reqWidth - img[0][0].getWidth() - img[2][0].getWidth() +1;
+        int horH = reqHeight - img[0][0].getHeight() - img[0][2].getHeight();
+         
+        // Create new (blank) image of required (scaled) size
+ 
+        BufferedImage scaledImage = new BufferedImage(reqWidth, reqHeight, BufferedImage.TYPE_INT_ARGB);
+ 
+        // Paint scaled version of image to new image
+        Graphics2D graphics2D = scaledImage.createGraphics();
+        graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+         
+        graphics2D.drawImage(img[0][0], 0, 0, img[0][0].getWidth(), img[0][0].getHeight(), null);
+        graphics2D.drawImage(img[1][0], img[0][0].getWidth(), 0, verW, img[0][0].getHeight(), null);
+        graphics2D.drawImage(img[2][0], img[0][0].getWidth()+verW, 0, img[0][2].getWidth(), img[0][0].getHeight(), null);
+         
+        graphics2D.drawImage(img[0][1], 0, img[0][0].getHeight(), img[0][0].getWidth(), horH, null);
+        graphics2D.drawImage(img[1][1], img[0][0].getWidth(), img[0][0].getHeight(), verW, horH, null);
+        graphics2D.drawImage(img[2][1], img[0][0].getWidth()+verW, img[0][0].getHeight(), img[0][2].getWidth(), horH, null);
+         
+        graphics2D.drawImage(img[0][2], 0, img[0][0].getHeight()+horH, img[0][0].getWidth(), img[2][0].getHeight(), null);
+        graphics2D.drawImage(img[1][2], img[0][0].getWidth(), img[0][0].getHeight()+horH, verW, img[2][0].getHeight(), null);
+        graphics2D.drawImage(img[2][2], img[0][0].getWidth()+verW, img[0][0].getHeight()+horH, img[0][2].getWidth(), img[2][0].getHeight(), null);
+ 
+        // clean up
+        graphics2D.dispose();
+         
+        return scaledImage;
+    }
+	
+	@Override
+	public void setPreferredSize(Dimension dimension) {
+		super.setPreferredSize(dimension);
+		this.buttonBg = new ImageIcon(load9Patch("ButtonBackground.png", (int)dimension.getWidth(), (int)dimension.getHeight()));
+		this.setIcon(this.buttonBg);
 	}
 
 	@Override
