7b7166a83f7e5e49b6c097a4c0499667ef7f02e3
DylanFayant <dylan.fayant@iut-valence.fr>
2016-06-02 12:21:01 +0200

    Add the base of the testStaff

---
+++ src/fr/iutvalence/info/dut/m2107/TestStaff.java
@@ -0,0 +1,67 @@
+package fr.iutvalence.info.dut.m2107;
+
+import junit.framework.TestCase;
+
+/**
+ * Test of the staff's class
+ * @author Projet Resto
+ */
+public class TestStaff extends TestCase {
+	
+	/**
+	 * Generates the test of the staff
+	 * @param name
+	 */
+	public TestStaff(String name)
+	{
+		super(name);
+	}
+	
+	/**
+	 * Test firstFreeIndex 
+	 */
+	public void testFirstFreeIndex()
+	{
+		
+	}
+	
+	/**
+	 * Test equals
+	 */
+	public void testEquals()
+	{
+		
+	}
+	
+	/**
+	 * Test deleteWaiter
+	 */
+	public void testDeleteWaiter()
+	{
+		
+	}
+	
+	/**
+	 * Test addWaiter
+	 */
+	public void testAddWaiter()
+	{
+		
+	}
+	
+	/**
+	 * Test editWaiter
+	 */
+	public void testEditWaiter()
+	{
+		
+	}
+	
+	/**
+	 * Test getWaiter
+	 */
+	public void testGetWaiter()
+	{
+		
+	}
+}
